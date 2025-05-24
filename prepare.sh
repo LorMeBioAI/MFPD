505d9e358da3f696bd0515241cfd6110093743cd
LorMeBioAI <103401277+LorMeBioAI@users.noreply.github.com>
2025-05-25 00:14:04 +0800

    Delete prepare.sh
    for new update

--- prepare.sh
+++
@@ -1,5 +0,0 @@
-#cd MFPD/
-mkdir -p db
-makeblastdb -in ./cluster/clades_95.fa -dbtype nucl -out ./db/clades_blastn_db
-mmseqs createdb ./cluster/clades_95.fa ./db/clades_mmseq_db
-mmseqs createindex ./db/clades_mmseq_db temp --search-type 3
